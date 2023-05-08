b2a007b92b416235bd126ee0ff818df00ed776ec
ADEKUNLE OLUWATOBI <88717213+TEAQUAN@users.noreply.github.com>
2023-05-08 19:44:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ var betweenGreater = [
     " I’m not telling you it is going to be easy- I am telling you it is going to be worth it.",
     "There's probably something there. Just make sure your presence is known so your feelings don't get hurt",
     "Dr. Love thinks that a relationship between " + "  " + yourNames + " and" + " " + yourPartnersName + " has a reasonable chance of working out, but on the other hand, it might not. Your relationship may suffer good and bad times. If things might not be working out as you would like them to, do not hesitate to talk about it with the person involved. Spend time together, talk with each other.",
-    "The chance of a relationship working out between" + yourNames + " " + "and" + " " + yourPartnersName + " is not very big, but a relationship is very well possible, if the two of you really want it to, and are prepared to make some sacrifices for it. You'll have to spend a lot of quality time together. You must be aware of the fact that this relationship might not work out at all, no matter how much time you invest in it."
+    "The chance of a relationship working out between " + yourNames + " " + "and" + " " + yourPartnersName + " is not very big, but a relationship is very well possible, if the two of you really want it to, and are prepared to make some sacrifices for it. You'll have to spend a lot of quality time together. You must be aware of the fact that this relationship might not work out at all, no matter how much time you invest in it."
 ];
 var randomItems = betweenGreater[Math.floor(Math.random() * betweenGreater.length)];
 
